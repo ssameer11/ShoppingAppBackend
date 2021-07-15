5a13ba753d6d3bfd99690386962aa6a7bf2aee93
Sameer Shaikh <sshaikh77096656@gmail.com>
2021-07-15 15:26:09 +0530

    First Version

--- src/keys/keys.ts
+++
@@ -1,8 +0,0 @@
-export const MONGOOSE_DATABASE_KEY = 'mongodb+srv://Sameer:akagami@cluster0.1gcl6.mongodb.net/OutfitsShop?retryWrites=true&w=majority';
-export const STRIPE_KEY = 'sk_test_51HuEHPA0aJg8piPjDICr8Zp5iXUgYHWCcFYr1FzUM8WmPaiEmhpx8NQ0yEqUcHQfGkG6wzwyylKomIZpLMD2ntFG00NlA6DSNe';
-export const SG_MAIL_KEY = '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';
-export const SENDGRID_EMAIL = 'sshaikh77096656@gmail.com';
-// ?retryWrites=true&w=majority
-
-
-// "test": "nodemon --ext ts --exec 'jasmine-ts  --config=./spec/support/jasmine.json'",
